d07b2df08625bfbfef9f77e0687293ecd1657a80
briannakara <bkarasha2@gmail.com>
2022-05-18 18:10:21 -0700

    section 8

---
+++ section-8/indexes_and_list_slicing.py
@@ -0,0 +1,13 @@
+nums = [[0, 2], [4, 6], [8, 10], [12, 14]]
+print(nums[0])
+print(nums[3][1])
+
+furniture = ["chairs", "table", "desk", "lamp", "bed"]
+print(furniture[-5])
+
+print("Most people own at least {} {}.".format(nums[0][1], furniture[-5]))
+
+decimals = [0.98, 8.76, 6.54, 4.32]
+print(decimals[1:])
+print(decimals[1:3])
+print(decimals[:2])
---
+++ section-8/intro_to_lists_ex.py
@@ -0,0 +1,4 @@
+uno =  [5, 30.67, "hello",  True, [1, 2, 3]]
+idk = list("brianna")
+print("e" in idk)
+print("a" not in idk)
